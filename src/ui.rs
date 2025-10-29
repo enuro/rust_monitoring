9471913794a955e3d627076a381e161714c13d43
Enuro <alpha9a@mail.ru>
2025-10-29 11:31:40 +0300

    Добавление отслеживания нагрузки диска и процесса(процесс и диск работают не особо корректно)

--- src/ui.rs
+++ src/ui.rs
@@ -6,9 +6,9 @@ use ratatui::style::{Color, Style, Stylize};
 use ratatui::text::Text;
 use ratatui::widgets::{Block, BorderType, Borders, Paragraph, Wrap};
 use sysinfo::{
-    Components, Disks, Networks, System,
+    Components, Disks, Networks, ProcessRefreshKind, ProcessesToUpdate, System, Pid
 };
-use sysinfo::{CpuRefreshKind, RefreshKind, MemoryRefreshKind};
+use sysinfo::{CpuRefreshKind, RefreshKind, MemoryRefreshKind, DiskRefreshKind};
 use crate::app::App;
 
 pub fn ui(frame: &mut Frame, app: &App) {
@@ -18,11 +18,13 @@ pub fn ui(frame: &mut Frame, app: &App) {
     let mut system_memory = System::new_with_specifics(
         RefreshKind::nothing().with_memory(MemoryRefreshKind::everything()),
     );
+    let mut system_disk = Disks::new_with_refreshed_list_specifics(DiskRefreshKind::everything());
+    let mut system_process = System::new_with_specifics(
+        RefreshKind::nothing().with_processes(ProcessRefreshKind::everything())
+    );
     
     std::thread::sleep(sysinfo::MINIMUM_CPU_UPDATE_INTERVAL);
-    system_cpu.refresh_cpu_usage();
-    system_memory.refresh_memory();
-
+    
     let chunks = Layout::default()
         .direction(Direction::Horizontal)
         .constraints([
@@ -33,6 +35,7 @@ pub fn ui(frame: &mut Frame, app: &App) {
     let chunks_data = Layout::default()
         .direction(Direction::Vertical)
         .constraints([
+            Constraint::Percentage(20),
             Constraint::Percentage(20),
             Constraint::Percentage(20),
             Constraint::Percentage(20),
@@ -69,12 +72,20 @@ pub fn ui(frame: &mut Frame, app: &App) {
             Constraint::Percentage(90),
         ])
         .split(chunks_data[3]);
+    
+    let disk_chunks = Layout::default()
+        .direction(Direction::Horizontal)
+        .constraints([
+            Constraint::Percentage(15),
+            Constraint::Percentage(90)
+        ]).split(chunks_data[4]);
 
     let null = Block::default().borders(Borders::BOTTOM).title("").style(Style::default());
 
     let gpu = Block::default().borders(Borders::ALL).title("GPU").style(Style::default().yellow());
 
     let memory_data = Block::default().borders(Borders::ALL).title("MEMORY").style(Style::default().red());
+    system_memory.refresh_memory();
     let used_memory = system_memory.used_memory() as f64;
     let total_memory = system_memory.total_memory() as f64;
     let memory_inf = (used_memory / total_memory) * 100.0;
@@ -86,10 +97,26 @@ pub fn ui(frame: &mut Frame, app: &App) {
     frame.render_widget(null.clone(), memory_chunks[1]);
 
     let net = Block::default().borders(Borders::ALL).title("NET").style(Style::default().magenta());
-    let tusk = Block::default().borders(Borders::ALL).title("TUSK").style(Style::default().blue());
+   
 
+    let disk_data = Block::default().borders(Borders::ALL).title("DISK").style(Style::default().cyan());
+    let mut used_disk: u64 = 0;
+    let mut total_disk: u64 = 0;
+    for disk in system_disk.list_mut() {
+        total_disk = total_disk + (disk.total_space() / 1024 / 1024 / 1024);
+        used_disk = used_disk + (disk.usage().written_bytes / 1024 / 1024 / 1024);
+        disk.refresh();
+    }
+    let disk_inf = (used_disk as f64 / total_disk as f64) * 100.0;
+    let disk = Paragraph::new(Text::styled(
+        format!("{}\n{:.1}%", "▄▄▄▄▄", disk_inf),
+        Style::default().fg(Color::Green).add_modifier(Modifier::BOLD)
+    )).block(disk_data).alignment(Alignment::Center).wrap(Wrap { trim: true });
+    frame.render_widget(disk, disk_chunks[0]);
+    frame.render_widget(null.clone(), disk_chunks[1]);
 
     let cpu_data = Block::default().borders(Borders::ALL).title("CPU").style(Style::default().green());
+    system_cpu.refresh_cpu_usage();
     let cpu_inf = system_cpu.global_cpu_usage();
     let cpu = Paragraph::new(Text::styled(
         format!("{}\n{:.1}%", "▄▄▄▄▄", cpu_inf),
@@ -100,11 +127,19 @@ pub fn ui(frame: &mut Frame, app: &App) {
 
 
     frame.render_widget(null.clone(), chunks[0]);
+    
     frame.render_widget(gpu.clone(), gpu_chunks[0]);
     frame.render_widget(null.clone(), gpu_chunks[1]);
 
     frame.render_widget(net.clone(), net_chunks[0]);
     frame.render_widget(null.clone(), net_chunks[1]);
-    frame.render_widget(tusk.clone(), chunks_data[4]);
-
+    
+    let tusk = Block::default().borders(Borders::ALL).title("PROCESS").style(Style::default().blue());
+    system_process.refresh_processes_specifics(ProcessesToUpdate::All, true, ProcessRefreshKind::everything());
+    let process_info = system_process.process(Pid::from(0));
+    let process = Paragraph::new(Text::styled(
+        format!("{:?}: {:.1}", process_info.unwrap().name(), 0.1),
+        Style::default().fg(Color::Green).add_modifier(Modifier::BOLD)
+    )).block(tusk).alignment(Alignment::Center).wrap(Wrap { trim: true });
+    frame.render_widget(process, chunks_data[5]);   
 }
